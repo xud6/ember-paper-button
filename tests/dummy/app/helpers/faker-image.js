44ead8c857c09761448344159a4bbf340ab77ef3
徐岱青 <email@xdq.me>
2026-06-21 20:20:03 +0800

    从demo应用中移除除PaperButton外所有的内容

--- tests/dummy/app/helpers/faker-image.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { faker } from '@faker-js/faker';
-
-export default helper(function fakerImage(positional, named) {
-  const options = { height: 200, width: 200, ...named };
-  return faker.image.url(options);
-});
